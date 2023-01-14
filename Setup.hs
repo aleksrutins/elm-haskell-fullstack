c99a377601d60d3d96104c40c0f47079bc701b3e
Aleks Rutins <aleks@rutins.com>
2023-01-14 16:13:15 -0500

    Get Elm basically building

--- Setup.hs
+++ Setup.hs
@@ -1,10 +1,23 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE Strict #-}
 import Distribution.Simple
-import System.Process (createProcess)
+import Distribution.Simple.Setup
+import Distribution.Types.PackageDescription
+import Distribution.Types.LocalBuildInfo
+import System.Process (callCommand)
 import Data.List
+import Data.Foldable
 
 main = defaultMainWithHooks $ simpleUserHooks {
-    postBuild = buildElm ["Main.elm"]
+    postBuild = buildElm ["Main", "About"]
 }
 
-buildElm files args buildFlags pkgDescription localBuildInfo = do
-    createProcess (proc "elm" ["make"] ++ (map ("frontend/" <>) files))
+buildElm :: [String]
+                -> Args
+                -> Distribution.Simple.Setup.BuildFlags
+                -> Distribution.Types.PackageDescription.PackageDescription
+                -> Distribution.Types.LocalBuildInfo.LocalBuildInfo
+                -> IO ()
+buildElm modules args buildFlags pkgDescription localBuildInfo =
+    for_ modules $ \mod -> do
+        callCommand $ "elm make frontend/" <> mod <> ".elm --optimize --output=frontend-dist/" <> mod <> ".js"
